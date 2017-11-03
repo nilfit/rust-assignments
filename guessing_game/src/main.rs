d9b0ee370d6a7f0fb95b655b64aa453bf828aa90
nilfit <nils.fitinghoff@gmail.com>
2017-11-03 18:02:20 +0100

    add guess history

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -8,12 +8,15 @@ fn main() {
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1,101);
     let mut tries = 0;
+    let mut guesses: Vec<(u32, String)> = Vec::new();
     loop {
         println!("Please input your guess.");
-        let mut guess = String::new();
-        io::stdin().read_line(&mut guess)
+        // _s to prevent shadowing so we can add the String to guesses later
+        // why not just store the u32?
+        let mut guess_s = String::new();
+        io::stdin().read_line(&mut guess_s)
             .expect("Failed to read line");
-        let guess: u32 = match guess.trim().parse() {
+        let guess: u32 = match guess_s.trim().parse() {
             Ok(num) => num,
             Err(_) => {
                 println!("Please type a number!");
@@ -22,6 +25,7 @@ fn main() {
         };
         // only increment tries for valid guesses
         tries = tries + 1;
+        guesses.push((tries, guess_s));
         println!("You guessed {} on try {}", guess, tries);
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
@@ -35,5 +39,9 @@ fn main() {
                 break;
             }
         }
+    }
+    println!("Your guesses:");
+    for &(n, ref guess) in guesses.iter() {
+        println!("{}\t{}", n, guess.trim());
     }
 }
